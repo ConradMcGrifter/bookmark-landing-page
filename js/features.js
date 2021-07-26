f50c3452e73681440879f2ba5330bbb90499bb03
skf <79630413+ConradMcGrifter@users.noreply.github.com>
2021-07-26 03:31:02 -0400

    finally got the tab red line working with pseudo elements

--- js/features.js
+++ js/features.js
@@ -18,32 +18,51 @@ const toggleTabs = (img, text) => {
     document.getElementById(text).classList.add("active");
 };
 
-const targetTab = () => {
+// const targetTab = () => {
+//     for (let i = 0; i < tabs.length; i++) {
+//         tabs[i].classList.add("inactive");
+//         tabs[i].addEventListener("click", function () {
+//             for (let j = 0; j < tabs.length; j++) {
+//                 tabs[j].classList.remove("active");
+//                 this.classList.add("active");
+//             }
+//         });
+//     }
+// };
+
+// function targetTab() {
+//     for (let i = 0; i < tabs.length; i++) {
+//         tabs[i].classList.remove("active");
+//         tabs[i].addEventListener("click", function () {
+//             this.classList.add("active");
+//         });
+//     }
+// }
+
+function targetTab(tab) {
     for (let i = 0; i < tabs.length; i++) {
-        tabs[i].classList.add("inactive");
-        tabs[i].addEventListener("click", function () {
-            for (let j = 0; j < tabs.length; j++) {
-                tabs[j].classList.remove("active");
-                this.classList.add("active");
-            }
-        });
+        tabs[i].classList.remove("active");
+        document.getElementById(tab).classList.add("active");
     }
-};
+}
 
 toggleTabs("simple-img", "simple-info");
 
-targetTab();
+// targetTab();
 document.querySelector(".default").classList.add("active");
 
 tab1.addEventListener("click", () => {
+    targetTab("simple--btn");
     toggleTabs("simple-img", "simple-info");
 });
 
 tab2.addEventListener("click", () => {
+    targetTab("speedy--btn");
     toggleTabs("speedy-img", "speedy-info");
 });
 
 tab3.addEventListener("click", () => {
+    targetTab("easy--btn");
     toggleTabs("easy-img", "easy-info");
 });
 // tab2.addEventListener("click", toggleTabs("speedy-img", "speedy-info"));
