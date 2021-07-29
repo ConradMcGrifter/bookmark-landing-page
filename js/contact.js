34a573dbd867e4641e237aafb1754cabf3a6e0fa
skf <79630413+ConradMcGrifter@users.noreply.github.com>
2021-07-29 18:32:10 -0400

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,13 +1,17 @@
 let input = document.querySelector(".contact__input");
+let icon = document.querySelector(".error-img");
+let inputWrap = document.querySelector(".input--wrap");
 
 input.value = "";
 
 document.querySelector(".contact__btn").addEventListener("click", function () {
     if (input.value == "" || !input.value.includes("@")) {
-        document.querySelector(".input--wrap").classList.add("error");
+        inputWrap.classList.add("error");
         input.classList.add("error");
+        icon.classList.add("active");
     } else {
-        document.querySelector(".input--wrap").classList.remove("error");
+        inputWrap.classList.remove("error");
         input.classList.remove("error");
+        icon.classList.remove("active");
     }
 });
