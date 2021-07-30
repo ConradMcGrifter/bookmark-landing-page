592b6ff936c3b8f14fbda2a0dbab42b4cbc4e9a6
skf <79630413+ConradMcGrifter@users.noreply.github.com>
2021-07-29 23:15:57 -0400

    forgot to add a class to toggle the social nav links
    since the social links on the navbar only appear on mobile i had to add a class to toggle them

--- js/navBar.js
+++ js/navBar.js
@@ -5,6 +5,7 @@ const hamburgerImg = document.querySelector(".hamburger-img");
 const navLinksWrap = document.querySelector(".nav__links--wrap");
 const logo = document.querySelector(".nav__img");
 const logoWhite = document.querySelector(".nav__img--white");
+const navSocialLinks = document.querySelector(".nav-social-links--wrap");
 
 const toggleMobileNav = () => {
     navLinksWrap.classList.toggle("active");
@@ -13,6 +14,7 @@ const toggleMobileNav = () => {
     body.classList.toggle("active");
     logo.classList.toggle("display");
     logoWhite.classList.toggle("display");
+    navSocialLinks.classList.toggle("display");
 
     if (!navLinksWrap.classList.contains("active")) {
         xBtn.style.display = "none";
