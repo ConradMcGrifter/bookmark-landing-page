f321022b7827777aaf92e71bdccf599c07309484
skf <79630413+ConradMcGrifter@users.noreply.github.com>
2021-07-29 22:08:24 -0400

    finished the footer
    done with all the sections. Now I want to go through and clean up the code / refactor

--- js/navBar.js
+++ js/navBar.js
@@ -3,12 +3,16 @@ const hamburger = document.getElementById("nav__hamburger");
 const xBtn = document.querySelector(".x-img");
 const hamburgerImg = document.querySelector(".hamburger-img");
 const navLinksWrap = document.querySelector(".nav__links--wrap");
+const logo = document.querySelector(".nav__img");
+const logoWhite = document.querySelector(".nav__img--white");
 
 const toggleMobileNav = () => {
     navLinksWrap.classList.toggle("active");
     hamburgerImg.style.display = "none";
     xBtn.style.display = "block";
     body.classList.toggle("active");
+    logo.classList.toggle("display");
+    logoWhite.classList.toggle("display");
 
     if (!navLinksWrap.classList.contains("active")) {
         xBtn.style.display = "none";
